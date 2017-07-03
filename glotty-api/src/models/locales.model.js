0162462bb45b021a2dd5bb0e750f553d625a225f
Oliver <owinter86@gmail.com>
2017-07-03 16:47:08 +0200

    Added Locales service in Mongoose

---
+++ glotty-api/src/models/locales.model.js
@@ -0,0 +1,17 @@
+// locales-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const locales = new Schema({
+    name: { type: String, required: true },
+    code: { type: String, required: true },
+    deleted: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+
+  return mongooseClient.model('locales', locales);
+};
