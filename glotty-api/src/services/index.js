27e1f0a0340038eb8433c41d0109a58f18edf43c
Oliver <owinter86@gmail.com>
2017-07-03 16:53:54 +0200

    Locales seed file added

--- glotty-api/src/services/index.js
+++ glotty-api/src/services/index.js
@@ -1,9 +1,11 @@
 const users = require('./users/users.service.js');
 const organizations = require('./organizations/organizations.service.js');
 const projects = require('./projects/projects.service.js');
+const locales = require('./locales/locales.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
   app.configure(organizations);
   app.configure(projects);
+  app.configure(locales);
 };
