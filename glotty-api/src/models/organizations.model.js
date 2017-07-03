fe1d467c5b123e8c54fdbbcdabd08b71c970694a
Oliver <owinter86@gmail.com>
2017-07-03 16:10:25 +0200

    Base Models added

---
+++ glotty-api/src/models/organizations.model.js
@@ -0,0 +1,15 @@
+// organizations-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const organizations = new Schema({
+    text: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+
+  return mongooseClient.model('organizations', organizations);
+};
--- glotty-api/src/services/index.js
+++ glotty-api/src/services/index.js
@@ -1,3 +1,9 @@
+const users = require('./users/users.service.js');
+const organizations = require('./organizations/organizations.service.js');
+const projects = require('./projects/projects.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(users);
+  app.configure(organizations);
+  app.configure(projects);
 };
