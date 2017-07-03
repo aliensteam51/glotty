a729383623f6204a9bd80c200b898cb16aff4c00
vanGalilea <z.galili@icloud.com>
2017-07-03 16:46:48 +0200

    Socket.io install and config

---
+++ glotty-app/src/api/client.js
@@ -0,0 +1,19 @@
+import feathers from 'feathers/client'
+import socketio from 'feathers-socketio/client'
+import hooks from 'feathers-hooks'
+import auth from 'feathers-authentication-client'
+import io from 'socket.io-client/dist/socket.io'
+
+const socket = io(host, {
+  transports: ['websocket']
+})
+
+const feathersClient = feathers()
+  .configure(hooks())
+  .configure(socketio(socket))
+  .configure(auth({
+    storage: window.localStorage,
+    storageKey: FEATHERS_TOKEN_KEY,
+  }))
+
+export default feathersClient
