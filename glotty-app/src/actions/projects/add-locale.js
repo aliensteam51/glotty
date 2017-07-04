0d375726c2a50998e8008c00dd2e80212d0d9d87
Hansterdam <hankortekaas@gmail.com>
2017-07-04 15:11:09 +0200

    Add actions that connect to the backend for adding platforms and locales to projects

---
+++ glotty-app/src/actions/projects/add-locale.js
@@ -0,0 +1,40 @@
+import API from '../../api'
+import {
+  APP_LOADING,
+  APP_DONE_LOADING,
+  LOAD_ERROR,
+  LOAD_SUCCESS
+ } from '../loading'
+
+export const ADDED_LOCALE = 'ADDED_LOCALE'
+
+const api = new API()
+
+export default (projectId, addLocale, platformCodes, localeCodes) => {
+  return (dispatch) => {
+    dispatch({ type: APP_LOADING })
+    dispatch({ type: LOAD_SUCCESS })
+    const backend = api.service('projects')
+    backend.patch(projectId, {
+      platformCodes: platformCodes,
+      localeCodes: localeCodes,
+      addLocale: addLocale
+    })
+      .then((result) => {
+        console.log(result)
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({ type: LOAD_SUCCESS })
+        dispatch({
+          type: ADDED_LOCALE,
+          payload: result
+        })
+      })
+      .catch((error) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({
+           type: LOAD_ERROR,
+           payload: error.message
+        })
+      })
+  }
+}
