3d580b0f3d0cc848829621276c4e3b0c1abfb5ec
Oliver <owinter86@gmail.com>
2017-07-04 12:07:01 +0200

    Created Nav Bar

--- glotty-app/src/projects/ProjectsContainer.js
+++ glotty-app/src/projects/ProjectsContainer.js
@@ -1,11 +1,31 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+import { Link } from 'react-router'
 
-class ProjectsContainer extends PureComponent {
+export class ProjectsContainer extends PureComponent {
+
+  // componentWillMount() {
+  //   const { fetchProjects } = this.props
+  //   fetchProjects()
+  // }
+
   render() {
     return (
-      <h1>ProjectsContainer temp.</h1>
+      <main className="container">
+        <Link to="#">
+          <h1>Project Container</h1>
+        </Link>
+      </main>
     )
   }
 }
 
-export default ProjectsContainer
+const mapStateToProps = ({currentUser }) => ({
+  currentUser,
+})
+
+export default connect(mapStateToProps, {
+  // fetchProjects,
+  push
+})(ProjectsContainer)
