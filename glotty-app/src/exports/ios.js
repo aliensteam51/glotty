1e7bc08e6a58c0558f9e788dc67ff09aadb84c23
vanGalilea <z.galili@icloud.com>
2017-07-04 18:21:27 +0200

    export function ios

---
+++ glotty-app/src/exports/ios.js
@@ -0,0 +1,11 @@
+export default(entries, localeCode) => {
+
+  const outputString = entries.reduce((str, entry) => {
+    const platform = entry.platforms.find((platf) => platf.platformCode === "ios")
+    const translation = platform.translations.find((tran) => tran.localeCode === localeCode)
+
+    return str + platform.key + " = " + translation.translation + ";\n"
+  }, "")
+
+  return outputString
+}
