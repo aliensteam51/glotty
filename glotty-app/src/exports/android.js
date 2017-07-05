95dcf0dbc90763d807bacb0b09c4891d59f3ad3c
vanGalilea <z.galili@icloud.com>
2017-07-05 10:53:15 +0200

    android, ios and json export modules update

--- glotty-app/src/exports/android.js
+++ glotty-app/src/exports/android.js
@@ -2,7 +2,10 @@ export default(entries, localeCode) => {
 
   const outputString = entries.reduce((str, entry) => {
     const platform = entry.platforms.find((platf) => platf.platformCode === "android")
+    if (!platform) return str + "No Android platform in this entry"
+
     const translation = platform.translations.find((tran) => tran.localeCode === localeCode)
+    if (!translation) return str + `No ${localeCode} translation in this entry`
 
     return str + `<string name="${platform.key}">${translation.translation}</string>\n`
   }, "<resources>")
--- glotty-app/src/exports/ios.js
+++ glotty-app/src/exports/ios.js
@@ -2,7 +2,10 @@ export default(entries, localeCode) => {
 
   const outputString = entries.reduce((str, entry) => {
     const platform = entry.platforms.find((platf) => platf.platformCode === "ios")
+    if (!platform) return str + "No ios platform in this entry"
+
     const translation = platform.translations.find((tran) => tran.localeCode === localeCode)
+    if (!translation) return str + `No ${localeCode} translation in this entry`
 
     return str + `"${platform.key}" = "${translation.translation}";\n`
   }, "")
