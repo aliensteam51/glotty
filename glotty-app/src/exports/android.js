5c9a139f15ec0910c660b832aeef5c466c00d2f5
vanGalilea <z.galili@icloud.com>
2017-07-07 11:43:33 +0200

    when there's no key or no translation in a specific language you get values from default platform

--- glotty-app/src/exports/android.js
+++ glotty-app/src/exports/android.js
@@ -2,15 +2,19 @@ import header from './header'
 
 export default(entries, localeCode) => {
   const outputString = entries.reduce((str, entry, index) => {
+    const defaultPlatform = entry.platforms.find((platf) => platf.platformCode === "default")
+
     let platform = entry.platforms.find((platf) => platf.platformCode === "android")
-    if (!platform) platform = entry.platforms.find((platf) => platf.platformCode === "default") //find default platform if android wasn't found
+    if (!platform) platform = defaultPlatform                                          //find default platform if android wasn't found
     if (!platform) return str + `No Android or default platforms were found in this entry\n`
 
-    const translation = platform.translations.find((tran) => tran.localeCode === localeCode)
-    if (!translation) return str + `No ${localeCode} translation in this entry\n` //a case like this hould not exist but still
+    let translation = platform.translations.find((tran) => tran.localeCode === localeCode)
+    if (translation.translation === '') translation = defaultPlatform.translations.find((tran) => tran.localeCode === localeCode) //find default platforms translation if androids translation wasn't found
+    if (!translation) return str + `No ${localeCode} translation in this entry\n`            //a case like this hould not exist but still
 
     let group = ''
     if (entries.length > index+1 && (entries[index+1].group !== entry.group)) group = `<!--${entries[index+1].group}-->\n\n` //checking if next entry doesnt have the same group
+    if (platform.keyId === '') platform = defaultPlatform // checking if keyId is empty than gets the keyId of default platform
 
     return str + `<string name="${platform.keyId}">${translation.translation}</string>\n` + group
   }, `<!--${header}--><resources>\n<!--${entries[0].group}-->`)
--- glotty-app/src/exports/i18n.js
+++ glotty-app/src/exports/i18n.js
@@ -1,11 +1,16 @@
 export default(entries, localeCode) => {
   const outputString = entries.reduce((str, entry) => {
+    const defaultPlatform = entry.platforms.find((platf) => platf.platformCode === "default")
+
     let platform = entry.platforms.find((platf) => platf.platformCode === "i18n")
-    if (!platform) platform = entry.platforms.find((platf) => platf.platformCode === "default") //find default platform if ios wasn't found
+    if (!platform) platform = defaultPlatform //find default platform if ios wasn't found
     if (!platform) return str + `No i18n platform in this entry\n`
 
-    const translation = platform.translations.find((tran) => tran.localeCode === localeCode)
+    let translation = platform.translations.find((tran) => tran.localeCode === localeCode)
+    if (translation.translation === '') translation = defaultPlatform.translations.find((tran) => tran.localeCode === localeCode)
     if (!translation) return str + `No ${localeCode} translation in this entry\n`
+
+    if (platform.keyId === '') platform = defaultPlatform
 
     const lastEntry =  entries[entries.length-1] !== entry ? `,\n` : `\n`
 
