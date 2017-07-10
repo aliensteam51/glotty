544293dd5cbb6c461784a903fa9ea5ffbc324cdb
Oliver <owinter86@gmail.com>
2017-07-10 14:39:09 +0200

    Created error alerts with auto close

--- glotty-app/src/reducers/loadError.js
+++ glotty-app/src/reducers/loadError.js
@@ -1,10 +1,11 @@
-import { LOAD_ERROR, LOAD_SUCCESS } from '../actions/loading'
+import { LOAD_ERROR, LOAD_SUCCESS, CLEAR_LOAD_ERROR } from '../actions/loading'
 
 export default (state = null, { type, payload } = {}) => {
   switch (type) {
     case LOAD_ERROR :
       return '' + payload
 
+    case CLEAR_LOAD_ERROR :
     case LOAD_SUCCESS :
       return null
 
